af1b85fcd286fc1580869cbf852ff162bd192d96
laerciodev <jose.laercio.bezerra@gmail.com>
2019-12-30 02:36:42 -0300

    Cleaner starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Laércio Blog`,
+    description: `A blog about frontend developer and other cool stuff`,
+    author: `@laerciodev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
