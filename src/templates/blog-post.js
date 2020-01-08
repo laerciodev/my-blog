910b9408c9c66265bb98260db45f2e4eebeb6a88
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2020-01-07 23:38:33 -0300

    Integrate layout to blogpost

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {  graphql } from 'gatsby';
+import { graphql } from 'gatsby';
+
+import Layout from '../components/Layout';
+import SEO from '../components/seo';
 
 const BlogPost = ({ data }) => {
     
     const post = data.markdownRemark;
 
     return (
-        <>
-            <h1>{ post.frontmatter.title }</h1>
+        <Layout>
+            <SEO title={ post.frontmatter.title } />
+            <h1>Title: { post.frontmatter.title }</h1>
             <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-        </>
+        </Layout>
     )
 }
 
